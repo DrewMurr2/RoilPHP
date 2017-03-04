69860e7064a9592b8133af479327710176f3ccf1
DrewMurr2 <DrewMurr2@gmail.com>
2017-03-04 17:15:30 -0600

    c

--- testSqlConnection.php
+++ testSqlConnection.php
@@ -10,20 +10,17 @@ if( $conn ) {
      die( print_r( sqlsrv_errors(), true));
 };
 
-$sql = "Select * From Users;";
+$sql = "Select email From Users;";
 $stmt = sqlsrv_query( $conn, $sql);
 if( $stmt === false ) {
      echo "no $stmt<br />";
      die( print_r( sqlsrv_errors(), true));
 }else{
      echo "In here<br />";
-    while( $obj = sqlsrv_fetch_object( $stmt)) {
-      foreach ($obj as $item) {
-                  $json = json_encode($item);
-      echo $json."<br />"; 
-      }
-
-}
+     $obj = sqlsrv_fetch_object( $stmt);
+      
+    $json = json_encode($obj);
+      echo $json."<br />";
 };
 
 
