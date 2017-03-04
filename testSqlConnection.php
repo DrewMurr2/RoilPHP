8798f4db2cb9e5e820cd821007934b263fb25ee7
DrewMurr2 <DrewMurr2@gmail.com>
2017-03-04 13:32:27 -0600

    Update testSqlConnection.php

--- testSqlConnection.php
+++ testSqlConnection.php
@@ -16,6 +16,7 @@ $stmt = sqlsrv_query( $conn, $sql, $params);
 if( $stmt === false ) {
      die( print_r( sqlsrv_errors(), true));
 }else{
- echo $stmt;
+     $json = json_encode($stmt)
+ echo $json;
 }
 ?>
